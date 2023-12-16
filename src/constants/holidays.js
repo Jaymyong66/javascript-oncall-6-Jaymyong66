95219ae26b4829aa69e4871cb41265702a6f345e
jayming66 <mjw8941@gmail.com>
2023-12-16 15:21:19 +0900

    feat : 법정 공휴일 데이터 추가

---
+++ src/constants/holidays.js
@@ -0,0 +1,19 @@
+const HOLIDAYS = Object.freeze({
+  HOLI_JANUARY: [{ day: 1, name: '신정' }],
+  HOLI_FEBRUARY: [],
+  HOLI_MARCH: [{ day: 1, name: '삼일절' }],
+  HOLI_APRIL: [],
+  HOLI_MAY: [{ day: 5, name: '어린이날' }],
+  HOLI_JUNE: [{ day: 6, name: '현충일' }],
+  HOLI_JULY: [],
+  HOLI_AUGUST: [{ day: 15, name: '광복절' }],
+  HOLI_SEPTEMBER: [],
+  HOLI_OCTOBER: [
+    { day: 3, name: '개천절' },
+    { day: 9, name: '한글날' },
+  ],
+  HOLI_NOVEMBER: [],
+  HOLI_DECEMBER: [{ day: 25, name: '성탄정' }],
+});
+
+export default HOLIDAYS;
